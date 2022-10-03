cd507a7675bd1a109fc095e44c321e192bd3586a
Ser9999 <mosiuksergiy@gmail.com>
2022-10-03 10:00:04 +0300

    add player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
 
+import Player from '@vimeo/player';
+console.log(player);
+// const throttle = require('lodash.throttle');
+
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+// const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+// const currentTime = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+// if (currentTime) {
+//   player.setCurrentTime(currentTime);
+// }
+
+// player.on('timeupdate', throttle(timeSave, 1000));
+// function timeSave() {
+//   player.getCurrentTime().then(function (seconds) {
+//     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(seconds));
+//   });
+// }
