af50cacfd43133f63f32a3263f304497bae795f5
Ser9999 <mosiuksergiy@gmail.com>
2022-10-03 10:37:37 +0300

    2-done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,21 +1,20 @@
 
 import Player from '@vimeo/player';
-console.log(player);
-// const throttle = require('lodash.throttle');
+const throttle = require('lodash.throttle');
 
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe);
-// const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
-// const currentTime = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+const currentTime = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
 
-// if (currentTime) {
-//   player.setCurrentTime(currentTime);
-// }
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
 
-// player.on('timeupdate', throttle(timeSave, 1000));
-// function timeSave() {
-//   player.getCurrentTime().then(function (seconds) {
-//     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(seconds));
-//   });
-// }
+player.on('timeupdate', throttle(timeSave, 1000));
+function timeSave() {
+  player.getCurrentTime().then(function (seconds) {
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(seconds));
+  });
+}
